16348ac57402fda776eec8612a000cda10d4bbe7
Paperback <sam@samwhat.com>
2021-09-24 05:50:25 +1000

    Changed tile resource

--- source/mvt/tile.gd
+++ source/mvt/tile.gd
@@ -4,8 +4,3 @@ export(Resource) var layer: Resource
 export(Vector2) var position := Vector2()
 export(int) var zoom: int
 export(Array) var geometry := []
-
-func _init(layer, x, y, z):
-	self.layer = layer
-	position = Vector2(x,y)
-	zoom = z
